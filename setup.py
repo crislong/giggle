4f29cf7ea84fceab3ac8bffad590b6ffad7bf366
Cristiano Longarini <81079965+crislong@users.noreply.github.com>
2021-05-11 16:11:12 +0200

    Add existing file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='chubbydisks',
+    packages=find_packages(include=['chubbydisks']),
+    version='0.0.1',
+    description='Library for massive disks',
+    author='Cristiano Longarini',
+    license='lol',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
